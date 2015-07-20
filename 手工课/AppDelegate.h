36e7a62b4d4b5b916d2efe8a51b9a753c6f36969
likethesuning <qianfeng@mpc.local>
2015-07-20 16:34:19 +0800

    love free
    a unFinished work

---
+++ 手工课/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手工课
+//
+//  Created by qianfeng on 15-7-14.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手工课/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手工课
+//
+//  Created by qianfeng on 15-7-14.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
